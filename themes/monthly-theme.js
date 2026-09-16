e8e718e4da1633c9340a7c2b26f039def73e1351
abivtherapy.coder <abivtherapy@gmail.com>
2026-09-16 13:08:24 -0600

    Ajustar septiembre v5.8 a la referencia visual móvil

--- themes/monthly-theme.js
+++ themes/monthly-theme.js
@@ -1,7 +1,7 @@
 (function initializeMonthlyTheme() {
   "use strict";
 
-  const THEME_VERSION = "5.7.0";
+  const THEME_VERSION = "5.8.0";
   const SEPTEMBER_CHARACTER = "assets/biometrimss-charro-septiembre-v1.png";
   const themes = [
     ["enero", "Año Nuevo", "✦"],
